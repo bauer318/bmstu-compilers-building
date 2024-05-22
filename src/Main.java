a4abfaba42075a4f1dc4dfb5b34996fa332cae63
jacques <jacqueskibamba@gmail.com>
2024-05-22 12:24:45 +0300

    base parser algo done, let fix Tree building

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,114 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Set<Nonterminal> nonterminals = new LinkedHashSet<>();
+        Set<Production> productions = new LinkedHashSet<>();
+        Set<Terminal> terminals = new LinkedHashSet<>();
+
+        var nonTerminalS = new Nonterminal("S", true);
+        var nonTerminalL = new Nonterminal("L");
+        var nonTerminalB = new Nonterminal("B");
+        var nonTerminalO = new Nonterminal("O");
+        var nonTerminalX = new Nonterminal("X");
+        var nonTerminalXPrime = new Nonterminal("X'");
+        var nonTerminalE = new Nonterminal("E");
+        var nonTerminalC = new Nonterminal("C");
+        var nonTerminalT = new Nonterminal("T");
+        var nonTerminalD = new Nonterminal("D");
+        var nonTerminalF = new Nonterminal("F");
+        var nonTerminalR = new Nonterminal("R");
+        var nonTerminalA = new Nonterminal("A");
+        var nonTerminalM = new Nonterminal("M");
+        var nonTerminalLPrime = new Nonterminal("L'");
+        var nonTerminalEPrime = new Nonterminal("E'");
+        var nonTerminalTPrime = new Nonterminal("T'");
+
+
+        Collections.addAll(nonterminals, nonTerminalS, nonTerminalL, nonTerminalB,
+                nonTerminalO, nonTerminalX, nonTerminalXPrime, nonTerminalE, nonTerminalC,
+                nonTerminalT, nonTerminalD, nonTerminalF, nonTerminalR, nonTerminalA, nonTerminalM,
+                nonTerminalLPrime, nonTerminalEPrime, nonTerminalTPrime);
+
+        var terminalBegin = new Terminal("begin", "begin");
+        var terminalEnd = new Terminal("end", "end");
+        var terminalVar = new Terminal("var", "var");
+        var terminalIs = new Terminal(":=", "is");
+        var terminalSemicolon = new Terminal(";", "semicolon");
+        var terminalConst = new Terminal("const", "const");
+        var terminalLParen = new Terminal("(", "lParen");
+        var terminalRParen = new Terminal(")", "rParen");
+        var terminalMul = new Terminal("*", "MUL");
+        var terminalDiv = new Terminal("/", "DIV");
+        var terminalAdd = new Terminal("+", "ADD");
+        var terminalSub = new Terminal("-", "SUB");
+        var terminalLess = new Terminal("<", "L");
+        var terminalLessEqual = new Terminal("<=", "LE");
+        var terminalEqual = new Terminal("=", "E");
+        var terminalNotEqual = new Terminal("<>", "NE");
+        var terminalGreat = new Terminal(">", "G");
+        var terminalGreatEqual = new Terminal(">=", "GE");
+
+        var epsilon = new GrammarSymbol("£");
+
+        Collections.addAll(terminals, terminalBegin, terminalEnd,
+                terminalVar, terminalIs, terminalSemicolon, terminalConst,
+                terminalLParen, terminalRParen, terminalMul, terminalDiv, terminalAdd,
+                terminalLess, terminalEqual, terminalLessEqual, terminalNotEqual,
+                terminalGreat, terminalGreatEqual, terminalSub);
+
+
+        var productionS = ParserUtils.buildProduction(nonTerminalS, terminalBegin, nonTerminalL, terminalEnd);
+
+        var productionL = ParserUtils.buildProduction(nonTerminalL, nonTerminalO, nonTerminalB);
+        var productionB = ParserUtils.buildProduction(nonTerminalB, nonTerminalLPrime);
+        var productionBSc = ParserUtils.buildProduction(nonTerminalB, epsilon);
+        var productionO = ParserUtils.buildProduction(nonTerminalO, terminalVar, terminalIs, nonTerminalX);
+        var productionX = ParserUtils.buildProduction(nonTerminalX, nonTerminalE, nonTerminalXPrime);
+        var productionXPrime = ParserUtils.buildProduction(nonTerminalXPrime, nonTerminalR, nonTerminalE);
+        var productionXPrimeSc = ParserUtils.buildProduction(nonTerminalXPrime, epsilon);
+        var productionE = ParserUtils.buildProduction(nonTerminalE, nonTerminalT, nonTerminalC);
+        var productionC = ParserUtils.buildProduction(nonTerminalC, nonTerminalEPrime);
+        var productionCSc = ParserUtils.buildProduction(nonTerminalC, epsilon);
+        var productionT = ParserUtils.buildProduction(nonTerminalT, nonTerminalF, nonTerminalD);
+        var productionD = ParserUtils.buildProduction(nonTerminalD, nonTerminalTPrime);
+        var productionDSc = ParserUtils.buildProduction(nonTerminalD, epsilon);
+        var productionF = ParserUtils.buildProduction(nonTerminalF, terminalVar);
+        var productionFSc = ParserUtils.buildProduction(nonTerminalF, terminalConst);
+        var productionFTh = ParserUtils.buildProduction(nonTerminalF, terminalLParen, nonTerminalE, terminalRParen);
+        var productionRL = ParserUtils.buildProduction(nonTerminalR, terminalLess);
+        var productionRLE = ParserUtils.buildProduction(nonTerminalR, terminalLessEqual);
+        var productionRE = ParserUtils.buildProduction(nonTerminalR, terminalEqual);
+        var productionLPrime = ParserUtils.buildProduction(nonTerminalLPrime, terminalSemicolon, nonTerminalO, nonTerminalB);
+        var productionEPrime = ParserUtils.buildProduction(nonTerminalEPrime, nonTerminalA, nonTerminalT, nonTerminalC);
+        var productionTPrime = ParserUtils.buildProduction(nonTerminalTPrime, nonTerminalM, nonTerminalF, nonTerminalD);
+        var productionRNE = ParserUtils.buildProduction(nonTerminalR, terminalNotEqual);
+        var productionRG = ParserUtils.buildProduction(nonTerminalR, terminalGreat);
+        var productionRGE = ParserUtils.buildProduction(nonTerminalR, terminalGreatEqual);
+        var productionAAdd = ParserUtils.buildProduction(nonTerminalA, terminalAdd);
+        var productionASub = ParserUtils.buildProduction(nonTerminalA, terminalSub);
+        var productionMMul = ParserUtils.buildProduction(nonTerminalM, terminalMul);
+        var productionMDiv = ParserUtils.buildProduction(nonTerminalM, terminalDiv);
+
+        Collections.addAll(productions, productionS, productionL, productionB, productionBSc, productionO,
+                productionX, productionXPrime, productionXPrimeSc, productionE, productionC, productionCSc,
+                productionT, productionD, productionDSc, productionF, productionFSc, productionFTh,
+                productionRL, productionRLE, productionRE, productionRNE, productionRG, productionRGE, productionAAdd,
+                productionASub, productionMMul, productionMDiv, productionLPrime, productionEPrime, productionTPrime);
+
+
+        Grammar grammar = new Grammar(nonterminals, terminals, nonTerminalS, productions);
+
+        List<GrammarSymbol> input = new ArrayList<>();
+        Collections.addAll(input, terminalBegin, terminalVar, terminalIs, terminalConst,
+                terminalLess, terminalVar, terminalEnd);
+        Parser parser = new Parser(grammar, input);
+
+        var t = parser.parseS();
+        System.out.println(parser.getRoot());
+
+    }
+
+    private static void oldTest() {
+        Set<Nonterminal> nonterminals = new LinkedHashSet<>();
         Set<Terminal> terminals = new LinkedHashSet<>();
         Set<Production> productions = new LinkedHashSet<>();
 
@@ -20,7 +128,7 @@ public class Main {
         //var nonTerminalB = new Nonterminal("B");
         /*Collections.addAll(nonterminals, nonTerminalE, nonTerminalEPrime, nonTerminalT,
                 nonTerminalTPrime,nonTerminalF);*/
-        Collections.addAll(nonterminals, nonTerminalS,nonTerminalA);
+        Collections.addAll(nonterminals, nonTerminalS, nonTerminalA);
 
         /*var terminalId = new Terminal("id", "ID");
         var terminalPlus = new Terminal("+", "ADD");
@@ -53,7 +161,7 @@ public class Main {
         var productionFSc = ParserUtils.buildProduction(nonTerminalF, terminalId);*/
 
         var productionS = ParserUtils.buildProduction(nonTerminalS,
-                terminalC, nonTerminalA,terminalD);
+                terminalC, nonTerminalA, terminalD);
         /*var productionSSc = ParserUtils.buildProduction(nonTerminalS,
                 terminalB);*/
 
@@ -68,10 +176,10 @@ public class Main {
         Collections.addAll(productions, productionS, productionA, productionASc);
 
 
-        Grammar grammar = new Grammar(nonterminals, terminals,nonTerminalS, productions);
+        Grammar grammar = new Grammar(nonterminals, terminals, nonTerminalS, productions);
 
         List<GrammarSymbol> input = new ArrayList<>();
-        Collections.addAll(input, terminalC,terminalA,terminalD);
+        Collections.addAll(input, terminalC, terminalA, terminalD);
         Parser parser = new Parser(grammar, input);
         /*var a  = parser.next();
         var b = parser.next();
@@ -91,7 +199,7 @@ public class Main {
 
         var b = followSetComputer.getFollowSets();*/
 
-       //var firsts = ParserUtils.computesFirst(nonterminals, terminals, productions);
+        //var firsts = ParserUtils.computesFirst(nonterminals, terminals, productions);
 
     }
 }
--- src/ru/bmstu/kibamba/parsing/Parser.java
+++ src/ru/bmstu/kibamba/parsing/Parser.java
@@ -26,38 +26,294 @@ public class Parser {
     private boolean S() {
         root = new TreeNode(grammar.getStart().getName());
         var a = next();
-        if (a.equals(new Terminal("c", "c"))) {
+        if (a.equals(new Terminal("begin", "begin"))) {
             currentIndex++;
-            root.addChild(new TreeNode("c"));
-            if (A()) {
-                currentIndex++;
+            root.addChild(new TreeNode("begin"));
+            if (L()) {
                 a = next();
-                if (a.equals(new Terminal("d", "d"))) {
-                    root.addChild(new TreeNode("d"));
+                if (a.equals(new Terminal("end", "end"))) {
+                    root.addChild(new TreeNode("end"));
                     return true;
                 } else {
                     erFlag++;
-                    System.out.println("ERROR 1 expected d but found " + a.getName());
+                    printError(1, "end", a.getName());
                     return false;
                 }
             } else {
-                System.out.println("ERROR expected A but found " + a.getName());
+                printError(2, "L", a.getName());
             }
         }
+        return false;
+    }
+
+    private boolean L() {
+        TreeNode lNode = new TreeNode("L");
+        var a = next();
+        if (O()) {
+            lNode.addChild(new TreeNode("O"));
+            if (B()) {
+                lNode.addChild(new TreeNode("B"));
+                root.addChild(lNode);
+                return true;
+            } else {
+                printError(3, " B after O ", a.getName());
+                return false;
+            }
+        }
+        printError(4, "O", a.getName());
+        return false;
+    }
+
+    private boolean B() {
+        TreeNode bNode = new TreeNode("B");
+        root.addChild(bNode);
+        if (LPrime()) {
+            bNode.addChild(new TreeNode("L'"));
+            return true;
+        }
+        bNode.addChild(new TreeNode("£"));
+        return true;
+    }
+
+    private boolean O() {
+        TreeNode oNode = new TreeNode("O");
+        var a = next();
+        if (a.equals(new Terminal("var", "var"))) {
+            oNode.addChild(new TreeNode("var"));
+            currentIndex++;
+            a = next();
+            if (a.equals(new Terminal(":=", "is"))) {
+                oNode.addChild(new TreeNode(":="));
+                currentIndex++;
+                if (X()) {
+                    oNode.addChild(new TreeNode("X"));
+                    root.addChild(oNode);
+                    return true;
+                } else {
+                    printError(6, "X", a.getName());
+                    return false;
+                }
+            } else {
+                printError(7, ":=", a.getName());
+                return false;
+            }
+        }
+        printError(8, "var", a.getName());
+        return false;
+    }
+
+    private boolean LPrime() {
+        TreeNode lPrimeNode = new TreeNode("L'");
+
+        var a = next();
+        if (a.equals(new Terminal(";", "semicolon"))) {
+            lPrimeNode.addChild(new TreeNode(";"));
+            currentIndex++;
+            if (O()) {
+                lPrimeNode.addChild(new TreeNode("O"));
+                if (B()) {
+                    lPrimeNode.addChild(new TreeNode("B"));
+                    root.addChild(lPrimeNode);
+                    return true;
+                } else {
+                    printError(9, "B", a.getName());
+                    return false;
+                }
+            } else {
+                printError(10, "O", a.getName());
+                return false;
+            }
+        }
+        printError(11, ";", a.getName());
+        return false;
+    }
+
+    private boolean EPrime() {
+        TreeNode ePrimeNode = new TreeNode("E'");
+
+        if (A()) {
+            ePrimeNode.addChild(new TreeNode("A"));
+            if (T()) {
+                ePrimeNode.addChild(new TreeNode("T"));
+                if (C()) {
+                    ePrimeNode.addChild(new TreeNode("C"));
+                    root.addChild(ePrimeNode);
+                    return true;
+                }
+                printError(23, "C", "Others ");
+                return false;
+            }
+            printError(24, "T", "Others ");
+            return false;
+        }
+        printError(25, "A", "Others ");
+        return false;
+    }
+
+    private boolean TPrime() {
+        TreeNode tPrimeNode = new TreeNode("T'");
+
+        if (M()) {
+            tPrimeNode.addChild(new TreeNode("M"));
+            if (F()) {
+                tPrimeNode.addChild(new TreeNode("F"));
+                if (D()) {
+                    tPrimeNode.addChild(new TreeNode("D"));
+                    root.addChild(tPrimeNode);
+                    return true;
+                }
+                printError(27, "D", "Others ");
+                return false;
+            }
+            printError(28, "F", "Others ");
+            return false;
+        }
+        printError(29, "M", "Others ");
+        return false;
+    }
+
+    private boolean X() {
+        TreeNode xNode = new TreeNode("X");
+        var a = next();
+        if (E()) {
+            xNode.addChild(new TreeNode("E"));
+            if (XPrime()) {
+                xNode.addChild(new TreeNode("X'"));
+                root.addChild(xNode);
+                return true;
+            }
+            printError(12, "X'", a.getName());
+
+        }
+        printError(13, "E", a.getName());
+        return false;
+    }
+
+    private boolean XPrime() {
+        TreeNode xPrimeNode = new TreeNode("X'");
+        if (R()) {
+            xPrimeNode.addChild(new TreeNode("R"));
+            if (E()) {
+                xPrimeNode.addChild(new TreeNode("E"));
+                root.addChild(xPrimeNode);
+                return true;
+            }
+            printError(14, "E", "Others ");
+            return false;
+        }
+        xPrimeNode.addChild(new TreeNode("£"));
+        root.addChild(xPrimeNode);
+        return true;
+    }
+
+    private boolean E() {
+        TreeNode eNode = new TreeNode("E");
+        if (T()) {
+            eNode.addChild(new TreeNode("T"));
+            if (C()) {
+                eNode.addChild(new TreeNode("C"));
+                root.addChild(eNode);
+                return true;
+            }
+            printError(15, "C", "Others ");
+            return false;
+        }
+        printError(16, "T", "Others ");
+        return false;
+    }
+
+    private boolean C() {
+        TreeNode cNode = new TreeNode("C");
+        root.addChild(cNode);
+        if (EPrime()) {
+            cNode.addChild(new TreeNode("E'"));
+            return true;
+        }
+        cNode.addChild(new TreeNode("£"));
+        return true;
+    }
+
+    private boolean T() {
+        TreeNode tNode = new TreeNode("T");
+        if (F()) {
+            tNode.addChild(new TreeNode("F"));
+            if (D()) {
+                tNode.addChild(new TreeNode("D"));
+                root.addChild(tNode);
+                return true;
+            }
+            printError(17, "D", "Others ");
+            return false;
+        }
+        printError(18, "F", "Others ");
+        return false;
+    }
+
+    private boolean D() {
+        TreeNode dNode = new TreeNode("D");
+        if (TPrime()) {
+            dNode.addChild(new TreeNode("T'"));
+            root.addChild(dNode);
+            return true;
+        }
+        dNode.addChild(new TreeNode("£"));
+        return true;
+    }
+
+    private boolean F() {
+        TreeNode fNode = new TreeNode("F");
+
+        var a = next();
+        if (a.equals(new Terminal("var", "var"))) {
+            currentIndex++;
+            fNode.addChild(new TreeNode("var"));
+            root.addChild(fNode);
+            return true;
+        }
+
+        if (a.equals(new Terminal("const", "const"))) {
+            currentIndex++;
+            fNode.addChild(new TreeNode("const"));
+            root.addChild(fNode);
+            return true;
+        }
+
+        if (a.equals(new Terminal("(", "lParen"))) {
+            fNode.addChild(new TreeNode("("));
+            currentIndex++;
+            if (E()) {
+                fNode.addChild(new TreeNode("E"));
+                a = next();
+                if (a.equals(new Terminal(")", "rParen"))) {
+                    currentIndex++;
+                    fNode.addChild(new TreeNode(")"));
+                    root.addChild(fNode);
+                    return true;
+                }
+                printError(19, ")", a.getName());
+                return false;
+            }
+            printError(20, "E", "Others ");
+            return false;
+        }
+
+        printError(21, "var , const or (E)", "Others ");
         return false;
     }
 
     private boolean M() {
         var a = next();
         TreeNode mNode = new TreeNode("M");
-        root.addChild(mNode);
+
         if (a.equals(new Terminal("*", "MUL"))) {
             currentIndex++;
             mNode.addChild(new TreeNode("*"));
+            root.addChild(mNode);
             return true;
         } else if (a.equals(new Terminal("/", "DIV"))) {
             currentIndex++;
             mNode.addChild(new TreeNode("/"));
+            root.addChild(mNode);
             return true;
         }
         printError(1, "* or /", a.getName());
@@ -84,20 +340,39 @@ public class Parser {
     private boolean R() {
         var a = next();
         TreeNode rNode = new TreeNode("R");
-        root.addChild(rNode);
+
         if (a.equals(new Terminal("<", "L"))) {
             currentIndex++;
             rNode.addChild(new TreeNode("<"));
+            root.addChild(rNode);
             return true;
         } else if (a.equals(new Terminal("<=", "LE"))) {
             currentIndex++;
             rNode.addChild(new TreeNode("<="));
+            root.addChild(rNode);
             return true;
         } else if (a.equals(new Terminal("=", "E"))) {
             currentIndex++;
             rNode.addChild(new TreeNode("="));
+            root.addChild(rNode);
+            return true;
+        } else if (a.equals(new Terminal("<>", "NE"))) {
+            currentIndex++;
+            rNode.addChild(new TreeNode("<>"));
+            root.addChild(rNode);
+            return true;
+        } else if (a.equals(new Terminal(">", "G"))) {
+            currentIndex++;
+            rNode.addChild(new TreeNode(">"));
+            root.addChild(rNode);
+            return true;
+        } else if (a.equals(new Terminal(">=", "GE"))) {
+            currentIndex++;
+            rNode.addChild(new TreeNode(">="));
+            root.addChild(rNode);
             return true;
         }
+        printError(22, "<, >, <=, >=, =,<>", a.getName());
         return false;
     }
 
